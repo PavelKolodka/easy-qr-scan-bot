423793e3e0158d18d450126014a27e9bc3685a52
HZ031189KPA <pavel.kolodka@a-bank.com.ua>
2025-04-17 17:44:01 +0300

    try to close app 1.2

--- web-app-launcher.py
+++ web-app-launcher.py
@@ -14,9 +14,10 @@ Basic example for a bot that uses inline keyboards. For an in-depth explanation,
 """
 
 import logging
-
+import requests
+import asyncio
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, WebAppInfo
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, CallbackContext, MessageHandler, filters
 
 from config import TOKEN, URL, URL_TEST
 
@@ -57,6 +58,25 @@ async def help_command(update: Update, context: CallbackContext) -> None:
     """Displays info on how to use the bot."""
     await update.message.reply_text("Type /start and open the QR dialog.")
 
+QR_API_URL = "https://cp.a-bank.com.ua/api/2/json/public/169430/2867dbd3e57dd80ca68772f0ba1272b7748f4758"
+
+async def send_qr_sync(data: str):
+    """Асинхронная обёртка для requests.post"""
+    loop = asyncio.get_running_loop()
+    response = await loop.run_in_executor(
+        None,
+        lambda: requests.post(QR_API_URL, json={"qr": data})
+    )
+    return response
+
+async def handle_web_app_data(update: Update, context: CallbackContext):
+    # вот тут данные из Vue
+    data = update.message.web_app_data.data
+    await update.message.reply_text(f"Получено из Vue: {data}")
+
+    response = await send_qr_sync(data)
+
+    await context.application.stop()
 
 def main() -> None:
     """Run the bot."""
@@ -67,7 +87,7 @@ def main() -> None:
     application.add_handler(CommandHandler('dev', develop))
     application.add_handler(CallbackQueryHandler(button))
     application.add_handler(CommandHandler('help', help_command))
-
+    application.add_handler(MessageHandler(filters.UpdateType.WEB_APP_DATA, handle_web_app_data))
     # Start the Bot
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
